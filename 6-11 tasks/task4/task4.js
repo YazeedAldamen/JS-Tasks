55dbdfa57018115ac9bb79d6e472cf26249b34be
YAZEED ALDAMEN <109088904+YazeedAldamen@users.noreply.github.com>
2022-11-07 09:15:45 +0300

    tasks

--- 6-11 tasks/task4/task4.js
+++ 6-11 tasks/task4/task4.js
@@ -1,21 +1,28 @@
-/* var sum = 0;
-for (let i=0; i <= 100; ){
+ var sum = 0;
+for (let i=1; i <= 100; i++ ){
     
-    document.write(sum);
-    document.write("<br/");
 
-    i++;
-    document.write(i)
+    
     sum= sum+i;
 
-} */
-let sum = 0;
+
+    document.write(sum);
+    document.write("<br/>")
+    if (sum>100){
+        break;
+    }
+   
+
+} 
+
+
+
+/* let sum = 0;
 let i = 0;
 while (sum< 100){
-
 
     document.write(sum)
     document.write("<br/>")
     i++;
     sum +=i;
-}
+} */
