867d7b468cb7488ab736f19af5854c9862ad53ac
YAZEED ALDAMEN <109088904+YazeedAldamen@users.noreply.github.com>
2022-11-08 09:17:02 +0300

    tasks

--- 7-11 tasks/task5/task5.js
+++ 7-11 tasks/task5/task5.js
@@ -2,10 +2,10 @@ let input=Number(prompt("insert a number"));
 
 function isOdd(num){
     if (num % 2 != 0){
-        document.write("true");
+        document.write("true it's odd");
     }
     else {
-        document.write("false");
+        document.write("false it's even");
 
     }
 }
